f35603a8aabdebf7468457860148d89a7c1cc263
alvexx45 <bernardoalves222@gmail.com>
2026-01-08 11:52:55 -0300

    Expense model

---
+++ src/main/java/com/bernardo/fcs/model/Expense.java
@@ -0,0 +1,114 @@
+package com.bernardo.fcs.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "expenses")
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String type;
+    private BigDecimal value;
+    private String p_method;
+    private LocalDate date;
+    
+    @ManyToOne
+    private User user;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+    @UpdateTimestamp
+    private Instant updateTimestamp;
+
+    public Expense() {}
+
+    public Expense(UUID id, String type, BigDecimal value, String p_method, LocalDate date, User user,
+            Instant creationTimestamp, Instant updateTimestamp) {
+        this.id = id;
+        this.type = type;
+        this.value = value;
+        this.p_method = p_method;
+        this.date = date;
+        this.user = user;
+        this.creationTimestamp = creationTimestamp;
+        this.updateTimestamp = updateTimestamp;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public String getP_method() {
+        return p_method;
+    }
+
+    public void setP_method(String p_method) {
+        this.p_method = p_method;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Instant getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(Instant creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+
+    public Instant getUpdateTimestamp() {
+        return updateTimestamp;
+    }
+
+    public void setUpdateTimestamp(Instant updateTimestamp) {
+        this.updateTimestamp = updateTimestamp;
+    }
+}
