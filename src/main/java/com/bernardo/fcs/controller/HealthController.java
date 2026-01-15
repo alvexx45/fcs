9bebacb8d5c167da20c2896ccfb279dcbf8aeafd
alvexx45 <bernardoalves222@gmail.com>
2026-01-15 18:27:48 -0300

    fix hc route

--- src/main/java/com/bernardo/fcs/controller/HealthController.java
+++ src/main/java/com/bernardo/fcs/controller/HealthController.java
@@ -1,12 +1,14 @@
 package com.bernardo.fcs.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
     
-    @GetMapping("/")
+    @GetMapping("")
     public String home() {
         return "FCS API is running!";
     }
